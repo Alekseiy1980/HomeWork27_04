1cd4b8086fee415c95e7cb050c37549f73d19d6e
Alekseiy1980 <129889765+Alekseiy1980@users.noreply.github.com>
2023-05-16 22:10:26 +0300

    Сортировка массива

---
+++ Metods/Program.cs
@@ -0,0 +1,31 @@
+﻿void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i]);
+    }
+    Console.WriteLine();
+}
+
+void SortArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < arr.Length; j++)
+        {
+            if (arr[minPosition] < arr[j]) minPosition = j;
+
+        }
+        int tmp = arr[i];
+        arr[i] = arr[minPosition];
+        arr[minPosition] = tmp;
+    }
+
+}
+int[] arr = { 1, 6, 8, 3, 1, 6, 9, 0, 2 };
+PrintArray(arr);
+SortArray(arr);
+Console.WriteLine();
+PrintArray(arr);
+
